d632d91c0c8933d2b797ceb42f4988b39f0eec72
siluuuuuu <61014829+siluuuuuu@users.noreply.github.com>
2020-02-13 14:46:45 +0100

    Add files via upload

---
+++ homepage/js/js.js
@@ -0,0 +1,41 @@
+function myFunction(){
+
+    
+          
+  //  document.getElementById(alpha).src = "img/01.jpg"; 
+    
+    window.open("img/tenor.gif");
+
+     }
+
+
+
+
+//  <button onclick="stupidFunction()"> <img src="pic_bulboff.gif" id = "myImg"/></button>
+
+// function stupidFunction(){
+//     document.getElementById(alpha).src = "img/avatar.png";
+// }
+
+// function myFunction(){
+          
+//     window.open("img/01.jpg");
+
+//     setTimeout(myFunction2 (), 2000);
+//      }
+
+
+//      function myFunction2(){
+//       window.open("img/02.jpg");
+//      }
+
+// function myFunction(){
+          
+//     window.open("img/01.jpg");
+
+//     // sleep(5000);
+
+//     window.close;
+
+//       window.open("img/02.jpg");
+//      }
